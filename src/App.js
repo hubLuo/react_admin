96af9d06ae32c12176c085bbbf8d58f406d1552e
hubLuo <hubluo@126.com>
2018-04-13 17:40:25 +0800

    add framework

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+// App.js
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import Content from './components/Content';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <SideBar />
+                <Content />
+            </div>
+        );
+    }
 }
 
 export default App;
